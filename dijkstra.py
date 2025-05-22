b64ad90e8247bc894bb2e3e93bd974425ebc0616
Gabriel Frigo <gfrigo.sena@gmail.com>
2025-05-22 12:27:01 -0300

    feat: djkstra algotithm

---
+++ dijkstra.py
@@ -0,0 +1,34 @@
+import heapq
+
+def dijkstra(graph, start):
+  min_heap = [(0, start)] # Início [(0, 'A')]
+  distances = {node: float('inf') for node in graph}
+  distances[start] = 0 # Distância 'A' conhecida
+
+  while min_heap:
+    current_distance, current_node = heapq.heappop(min_heap)
+
+    if current_distance > distances[current_node]:
+      continue
+
+    for neighbor, weight in graph[current_node].items():
+      distance = current_distance + weight
+
+      if distance < distances[neighbor]:
+        distances[neighbor] = distance
+        heapq.heappush(min_heap, (distance, neighbor))
+        0
+  return distances
+
+graph = {
+  'A': {'B': 2, 'C': 4},
+  'B': {'A': 2, 'C': 1, 'D': 7},
+  'C': {'A': 4, 'B': 1, 'D': 2},
+  'D': {'B': 7, 'C': 2, 'E': 2},
+  'E': {'C': 10, 'D': 2},
+}
+
+start = 'A'
+
+distances = dijkstra(graph, start)
+print(distances)
