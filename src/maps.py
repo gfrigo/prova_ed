502d1ebdf2610c46185df32e4b1793df841957b0
Gabriel Frigo <gfrigo.sena@gmail.com>
2025-05-23 21:46:57 -0300

    feat: data structure for djikstra

--- src/maps.py
+++ src/maps.py
@@ -7,11 +7,28 @@ from googlemaps.directions import directions
 # Google Maps Platform API_KEY
 load_dotenv()
 api_key = os.getenv("API_KEY")
-print(f"Google Maps API_KEY: {api_key} {type(api_key)}")
+print(f"Google Maps API_KEY: {api_key} {type(api_key)}\n")
 
+# Configuração do client, origem e destino
 client = Client(key=api_key)
 origin = "Av. Eng. Eusébio Stevaux, 823 - Santo Amaro, São Paulo - SP, 04696-000"
 destination = "Estádio Cícero Pompeu de Toledo - Morumbis"
 
-routes = directions(client, origin, destination)
-print(routes)
+# Obtendo rotas possíveis de origin -> destination
+routes = directions(client, origin, destination)[0]['legs'][0]
+
+# Dados obtidos
+distance = routes['distance']['text']
+duration = routes['duration']['text']
+end_address = routes['end_address']
+end_location = routes['end_location']
+start_address = routes['start_address']
+start_location = routes['start_location']
+            
+
+print(f"Distance: {distance}\n"
+      f"Duration: {duration}\n"
+      f"End Address: {end_address}\n"
+      f"End Location (lat/lng): {end_location}\n"
+      f"Start Eddress: {start_address}\n"
+      f"Start Location (lat/lng): {start_location}\n")
